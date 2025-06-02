ca78b18ae8cdec631a2b0fdd23c692ab4ae3b259
wds0809 <wds0809@gmail.com>
2025-06-02 18:16:48 +0000

    Movimentando as Peças do xadrez Desafio-Novato-Wanderson

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,28 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // MOVIMENTE A TORRE 5 CASAS A DIREITA
+    printf("=== TORRE ===\n"); // Imprimir o nome da peça
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n"); // Imprime a direção do movimento
+    }
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // MOVIMENTO DO BISPO 5 CASAS NA DIAGONAL PARA CIMA E Á DIREITA
+    int i = 1;
+    printf("\n=== BISPO ===\n"); // Imprimir o nome da peça
+    while (i <= 5){
+        printf("Cima, Direita\n"); // Imprime a direção do movimento
+        i++;
+    }
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // MOVIMENTO DA RAINHA 8 CASAS PARA Á ESQUERDA
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    i = 1;
+    printf("\n=== RAINHA ===\n"); // Imprimir o nome da peça
+    do{
+       printf("Esquerda\n", i); // Imprime a direção do movimento
+       i++;
+    } while (i <= 8);
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
